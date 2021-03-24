fe32a10bc41d0faac4e998eb10df9ecd27b3ebc7
Nguyen Quang Huy <trannguyenhan01092000@gmail.com>
2021-03-24 15:00:24 +0700

    lab2

--- lab2/Exercise.php
+++ lab2/Exercise.php
@@ -5,25 +5,25 @@
 	$hobbies = array();
 
 	$cat = $_POST["cat"];
-	if($cat == "Yes") array_push($hobbies,$cat);
+	if($cat == "Yes") array_push($hobbies,"Cat");
 
 	$cooking = $_POST["cooking"];
-	if($cat == "Yes") array_push($hobbies,$cat);
+	if($cooking == "Yes") array_push($hobbies,"Cooking");
 
 	$dog = $_POST["dog"];
-	if($dog == "Yes") array_push($hobbies,$dog);
+	if($dog == "Yes") array_push($hobbies,"Dog");
 
 	$math = $_POST["math"];
-	if($math == "Yes") array_push($hobbies,$math);
+	if($math == "Yes") array_push($hobbies,"Math");
 
 	$web = $_POST["web"];
-	if($web == "Yes") array_push($hobbies,$web);
+	if($web == "Yes") array_push($hobbies,"Web");
 
 	$facebook = $_POST["facebook"];
-	if($facebook == "Yes") array_push($hobbies,$facebook);
+	if($facebook == "Yes") array_push($hobbies,"Facebook");
 
 	$youtube = $_POST["youtube"];
-	if($youtube == "Yes") array_push($hobbies,$youtube);
+	if($youtube == "Yes") array_push($hobbies,"Youtube");
 
 	print "Hello, $name <br />";
 	print "You are studing at $class, $university <br />";
